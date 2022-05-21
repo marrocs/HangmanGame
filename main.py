ed5525e44ba8759d3b76c4013e2857112c6f68d1
Lucas Rocha <lucasrochamarques@gmail.com>
2022-05-21 09:47:49 -0300

    mudança na estrutura das perguntas e respostas e mais

--- main.py
+++ main.py
@@ -1,49 +1,63 @@
-from questions_and_answers import *
+'''v1 - bedc7c4 - 10/01/22 - problems and upgrades
+1 - Create functions to operate system
+2 - Win game message and system
+3 - Multiple letters in correct_answer'''
+
+from q_and_a import *
 from random import randint
-
-print('Welcome to Hangman Game!')
-
 
 def main():
 
-    diff = int(input('1 - Easy; 2 - Medium; 3 - Hard\nSelect difficult: '))
+    print('Welcome to Hangman Game!')
 
-    if diff == 1:
-        quest = diff_easy[randint(0, len(diff_easy)-1)]
-        answer_scope = '_' * len(quest[1])
-        answer_list = list('_' for _ in quest[1])
-        print(quest[0], '\n', answer_scope)
-        shots = 5
+    diff_selected = int(input('1 - EASY 2 - MEDIUM 3 - HARD \n SELECT DIFFICULT: '))
 
-        while shots > 0:
-            answer = input(f'You have {shots} shots! Try a letter: ')
+    # if diff_selected != 1 or 2 or 3:
+    #     print('Difficult not exist. Try again.')
+    #     diff_selected = int(input('1 - EASY 2 - MEDIUM 3 - HARD \n SELECT DIFFICULT: '))
 
-            if answer in str.lower(quest[1]):
-                print(f'There is "{answer}" in answer!')
-                indexes = []
+    sort_quest = randint(0, len(q_and_a[diff_selected - 1]) -1)
+    question_answer = q_and_a[diff_selected - 1][sort_quest]
+    question = question_answer[0]
+    correct_answer = [x for x in str.lower(question_answer[1])]
+    answer_scope = ['_' for _ in range(len(question_answer[1]))]
+    attempts = 5
 
-                for x in range(len(str.lower(quest[1]))):
-                    if str.lower(quest[1][x]) == answer:
-                        indexes.append(x)
+    print(question)
+    print(' '.join(answer_scope))
 
-                for i in indexes:
-                    answer_list[i] = answer
+    def attempt():
 
-                print(''.join(answer_list))
+        letter = input(f'You got {attempts} tries. Try a letter: ').lower()
 
-            else:
-                print('Wrong! Try again.')
-                shots -= 1
+        if letter in correct_answer:
+            count = 0
+            indexes = []
 
-        print("You've been hanged!")
+            print(f'There is "{letter}" in answer!')
 
-    elif diff == 2:
-        pass
-    elif diff == 3:
-        pass
-    else:
-        print('Difficult not reconized. Try again.')
-        main()
+            for x in correct_answer:
+                if x == letter:
+                    indexes.append(count)
+                count += 1
+
+            for idx, _ in enumerate(answer_scope):
+                if idx in indexes:
+                    answer_scope[idx] = letter
+            
+            print(' '.join(answer_scope))
+
+            attempt()
+        
+        else:
+
+            attempts -= 1
+            print(f'One out. You have {attempts} left.')
+
+            attempt()
+
+    print(attempt())
+
 
 if __name__ == '__main__':
     main()
---
+++ q_and_a.py
@@ -0,0 +1,14 @@
+q_and_a = [
+        [
+            ['Animal que não lava o pé', 'sapo'],
+            ['Maior roedor do mundo', 'capivara']
+        ],
+        [
+            ['Grupo de comédia britânica', 'Monty Python'],
+            ['Considerado o pai da sociologia', 'Emile Durkhein']
+        ],
+        [
+            ['Magnum opus de Paulo Freire', 'Pedagogia do oprimido'],
+            ['Marca de carros brasileira', 'Gurgel']
+        ]
+]
